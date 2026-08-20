85862306506213963fbf82b5353970265ce7994d
Bartosz <brotus@tutamail.com>
2026-08-20 17:59:24 +0200

    update/ deleted pitches.h

--- ledoforce/ledoforce.ino
+++ ledoforce/ledoforce.ino
@@ -1,59 +1,3 @@
-
-
-/*  NOT USED YET
-// -------------------------------------------------
-// Copyright (c) 2022 HiBit <https://www.hibit.dev>
-// -------------------------------------------------
-
-#include "pitches.h"
-
-#define BUZZER_PIN 9
-
-int melody[] = {
-  NOTE_AS4, NOTE_AS4, NOTE_AS4,
-  NOTE_F5, NOTE_C6,
-  NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,
-  NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,
-  NOTE_AS5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_C5, NOTE_C5, NOTE_C5,
-  NOTE_F5, NOTE_C6,
-  NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,
-
-  NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,
-  NOTE_AS5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_C5, NOTE_C5,
-  NOTE_D5, NOTE_D5, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5,
-  NOTE_F5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_D5, NOTE_E5, NOTE_C5, NOTE_C5,
-  NOTE_D5, NOTE_D5, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5,
-
-  NOTE_C6, NOTE_G5, NOTE_G5, REST, NOTE_C5,
-  NOTE_D5, NOTE_D5, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5,
-  NOTE_F5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_D5, NOTE_E5, NOTE_C6, NOTE_C6,
-  NOTE_F6, NOTE_DS6, NOTE_CS6, NOTE_C6, NOTE_AS5, NOTE_GS5, NOTE_G5, NOTE_F5,
-  NOTE_C6
-};
-
-int durations[] = {
-  8, 8, 8,
-  2, 2,
-  8, 8, 8, 2, 4,
-  8, 8, 8, 2, 4,
-  8, 8, 8, 2, 8, 8, 8,
-  2, 2,
-  8, 8, 8, 2, 4,
-
-  8, 8, 8, 2, 4,
-  8, 8, 8, 2, 8, 16,
-  4, 8, 8, 8, 8, 8,
-  8, 8, 8, 4, 8, 4, 8, 16,
-  4, 8, 8, 8, 8, 8,
-
-  8, 16, 2, 8, 8,
-  4, 8, 8, 8, 8, 8,
-  8, 8, 8, 4, 8, 4, 8, 16,
-  4, 8, 4, 8, 4, 8, 4, 8,
-  1
-};
-*/
-
 #include <MD_MAX72xx.h> 
 
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW 
@@ -62,19 +6,15 @@ int durations[] = {
 
 MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
-
 void drawLightsaber() {
   for (int i = 2; i < 32; i++) {
     for(int y = 2; y <7; y++){
       mx.setPoint(y, i, true);  
     }
     
-    
     delay(15); 
   }
 }
-
- 
 
 // FROM pomidoro project
 
@@ -89,7 +29,7 @@ void drawCharacter(int startX, int num, font_t used_font);
 int seconds = 1186;
 font_t used_font_num;
 
-https://xantorohara.github.io/led-matrix-editor/#001f1111111f0000|0011111f10100000|001d151515170000|00151515151f0000|00070404041f0000|00171515151d0000|001f1515151d0000|0001011905030000|001f1515151f0000|00171515151f0000
+//https://xantorohara.github.io/led-matrix-editor/#001f1111111f0000|0011111f10100000|001d151515170000|00151515151f0000|00070404041f0000|00171515151d0000|001f1515151d0000|0001011905030000|001f1515151f0000|00171515151f0000
 font_t FONT_NUM_VAD{
   7,
   6,
@@ -110,7 +50,8 @@ font_t FONT_NUM_VAD{
 
 void drawCharacter(int startX, int num, font_t used_font) {
   uint64_t character = used_font.characters[num];
-
+  
+  // AI MADE THIS:
   for (int row = (8 - used_font.height); row < 8; row++) {
     byte line = (character >> (row * 8)) & 0xFF;
     for (int col = 0; col < used_font.width; col++) {
@@ -118,6 +59,7 @@ void drawCharacter(int startX, int num, font_t used_font) {
       mx.setPoint(row, startX - col, pointInflamed);
     }
   }
+  // END OF AI
 }
 
 void drawSeparator() {
@@ -127,7 +69,6 @@ void drawSeparator() {
 
     mx.setPoint(3, 16, true);
     mx.setPoint(3, 17, true);
-  
   
 }
 void drawPomodoroTime(int seconds){  
